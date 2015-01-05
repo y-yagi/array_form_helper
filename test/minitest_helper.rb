f1f3ef518b0c64e03242093fc48ee26517012cfd
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-01-05 14:43:41 +0900

    clean up test helper

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,20 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'bundler/setup'
 require 'coveralls'
 Coveralls.wear!
 
-require 'support/person'
-require 'pry'
-require 'action_dispatch'
 require 'array_form_helper'
+require 'support/person'
 require 'minitest/autorun'
+require 'pry'
 
-def setup_controller
-  router = ActionDispatch::Routing::RouteSet.new
-  router.draw { resources :people }
-  @controller = ActionView::TestCase::TestController.new
-  @controller.instance_variable_set(:@_routes, router)
-  @controller.class_eval { include router.url_helpers }
-  @controller.view_context_class.class_eval { include router.url_helpers }
+module ArrayFormHelper::TestHelper
+  def setup_controller
+    router = ActionDispatch::Routing::RouteSet.new
+    router.draw { resources :people }
+    @controller = ActionView::TestCase::TestController.new
+    @controller.instance_variable_set(:@_routes, router)
+    @controller.class_eval { include router.url_helpers }
+    @controller.view_context_class.class_eval { include router.url_helpers }
+  end
 end
