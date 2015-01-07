11bc50eaadf93b59eb8a8b2b78fdb296ae744126
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-01-07 09:35:21 +0900

    support array version of `xxx_field` method

--- lib/array_form_helper/form_builder.rb
+++ lib/array_form_helper/form_builder.rb
@@ -1,8 +1,13 @@
+require 'array_form_helper/form_helper'
 module ArrayFormHelper
   module FormBuilder
-    def array_text_field(method, options = {})
-      options[:value] = @object.public_send(method)
-      @template.array_text_field(@object_name, method, objectify_options(options))
+    ArrayFormHelper::FormHelper::ARRAY_FIELD_HELPERS.each do |selector|
+      class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+        def array_#{selector}(method, options = {})
+          options[:value] = @object.public_send(method)
+          @template.array_text_field(@object_name, method, objectify_options(options))
+        end
+      RUBY_EVAL
     end
   end
 end
--- lib/array_form_helper/form_helper.rb
+++ lib/array_form_helper/form_helper.rb
@@ -1,17 +1,28 @@
 module ArrayFormHelper
   module FormHelper
-    def array_text_field(object_name, method, options = {})
-      options[:name] = "#{object_name}[#{method}][]"
-      if options[:array_size]
-        array_size = options.delete(:array_size).to_i
-        value = options.delete(:value) if options[:value]
-        array_size.times.map do |i|
-          options[:value] = value[i] if value && value.is_a?(Array)
-          text_field(object_name, method, options)
-        end.join.html_safe
-      else
-        text_field(object_name, method, options)
-      end
+    ARRAY_FIELD_HELPERS = [
+      :text_field, :text_area, :color_field, :search_field, :telephone_field,
+      :phone_field, :date_field, :time_field, :datetime_field,
+      :datetime_local_field, :month_field, :week_field, :url_field,
+      :email_field, :number_field, :range_field
+    ]
+
+    ARRAY_FIELD_HELPERS.each do |selector|
+      class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+        def array_#{selector}(object_name, method, options = {})
+          options[:name] = "%s[%s][]" % [object_name, method]
+          if options[:array_size]
+            array_size = options.delete(:array_size).to_i
+            value = options.delete(:value) if options[:value]
+            array_size.times.map do |i|
+              options[:value] = value[i] if value && value.is_a?(Array)
+              #{selector}(object_name, method, options)
+            end.join.html_safe
+          else
+            #{selector}(object_name, method, options)
+          end
+        end
+      RUBY_EVAL
     end
   end
 end
